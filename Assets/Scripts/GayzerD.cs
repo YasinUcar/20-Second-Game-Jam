0bcb88e2df05840f66fb5ee75eb5381530878a2e
Cebrail Akyüz <103183740+Cebrail000001F@users.noreply.github.com>
2022-11-05 21:15:33 +0300

    GayzerEffect

--- Assets/Scripts/GayzerD.cs
+++ Assets/Scripts/GayzerD.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class GayzerD : MonoBehaviour
 {
+    public GameObject effectObje2;
+    ParticleSystem ParticleSystem;
+    private void Start()
+    {
+        ParticleSystem = effectObje2.GetComponent<ParticleSystem>();
+    }
     private void OnTriggerStay(Collider other)   
     {
         if (Input.GetKey("d"))
         {
             other.GetComponent<Rigidbody>().AddForce(new Vector3(0 ,10,0), ForceMode.Impulse);
+            ParticleSystem.Play();
         }
     }
     
