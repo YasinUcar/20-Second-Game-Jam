01a599c0c02bdbc279593a4f4e8124e0ed9cc97b
Yasin Uçar <yasinucar@protonmail.com>
2022-11-06 11:40:04 +0300

    rg fix

--- Assets/Scripts/GayzerA.cs
+++ Assets/Scripts/GayzerA.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class GayzerA : MonoBehaviour
 {
-    public GameObject effectObje;
+    [SerializeField] GameObject effectObje;
     ParticleSystem ParticleSystem;
+ 
     private void Start()
     {
-        ParticleSystem= effectObje.GetComponent<ParticleSystem>();
+        ParticleSystem = effectObje.GetComponent<ParticleSystem>();
+       
     }
     private void OnTriggerStay(Collider other)
     {
-        if (Input.GetKey("a"))
+        if (Input.GetKey("a") && other.gameObject.CompareTag("Halka"))
         {
-            other.GetComponent<Rigidbody>().AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
+            other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
             ParticleSystem.Play();
         }
+        
     }
 }
